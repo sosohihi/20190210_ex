ed107ab2098fe7a9e7bafad53af4abca3063bdd1
sosohihi <chachavitamin@gmail.com>
2019-03-10 13:04:31 +0900

    20190310

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+
+router.get('/abc', function(req, res, next) {
+  console.log('안녕');
+  //res.send('hola');
+  res.render('new')
+});
+
+module.exports = router
+
+
+
+

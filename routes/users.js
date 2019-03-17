8e35aba871512dd1a092e20cac15a2482550bf67
sosohihi <chachavitamin@gmail.com>
2019-03-17 13:01:23 +0900

    20190317

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.get('/login', function(req, res, next) {
+  console.log('로긴ejs파일을 넘겨준다')
+  res.render('login',{title: 'Login Page'});
+
+  
+});
+
+
+router.post('/login', function(req, res, next) {
+  if(req.body.ID=='cat'){
+
+  
+  if(req.body.PW=='joa'){
+    res.send('Login 성공');
+  }
+  else{
+  res.send('Login 실패: PW오류');
+  }
+}
+else{
+  res.send('Login 실패: ID오류');
+}
+  
+});
+
+
 module.exports = router;
